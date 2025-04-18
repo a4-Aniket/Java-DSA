4badb5634130fe40c4043b490d2a400d4b2d8679
a4-Aniket <141369113+a4-Aniket@users.noreply.github.com>
2025-04-18 23:01:43 +0530

    Update RotatedBinarySearch.java

--- Kunal_DSA_Playlist/BinarySearch/RotatedBinarySearch.java
+++ Kunal_DSA_Playlist/BinarySearch/RotatedBinarySearch.java
@@ -1,5 +1,5 @@
 package Kunal_DSA_Playlist.BinarySearch;
-
+//This code is for the array contains no dublicate elements for non distinct elements in array this code will not work
 public class RotatedBinarySearch {
     public static void main(String[] args) {
         int[] arr = {4,5,6,7,0,1,2};
